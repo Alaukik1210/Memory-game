21368afac5face89a6c2e32fcbb60159e78070a0
Saif <mohammadsaif0847@gmail.com>
2024-07-11 12:56:31 +0530

    Color-Changed

--- script.js
+++ script.js
@@ -12,22 +12,22 @@ let firstcard = false;
 let secondCard = false;
 
 //item array
-const items =[
-  {name: "bee", image: "bee.png"},
-  {name: "crocodile", image: "crocodile.png"},
-  {name: "macaw", image: "macaw.png"},
-  {name: "gorilla", image: "gorilla.png"},
-  {name: "tiger", image: "tiger.png"},
-  {name: "monkey", image: "monkey.png"},
-  {name: "chameleon", image: "chameleon.png"},
-  {name: "anaconda", image: "anaconda.png"},
-  {name: "sloth", image: "sloth.png"},
-  {name: "cockatoo", image: "cockatoo.png"},
-  {name: "toucan", image: "toucan.png"},
+const items = [
+	{ name: 'bee', image: 'Images/bee.png' },
+	{ name: 'crocodile', image: 'Images/crocodile.png' },
+	{ name: 'macaw', image: 'Images/macaw.png' },
+	{ name: 'gorilla', image: 'Images/gorilla.png' },
+	{ name: 'tiger', image: 'Images/tiger.png' },
+	{ name: 'monkey', image: 'Images/monkey.png' },
+	{ name: 'chameleon', image: 'Images/chameleon.png' },
+	{ name: 'anaconda', image: 'Images/anaconda.png' },
+	{ name: 'sloth', image: 'Images/sloth.png' },
+	{ name: 'cockatoo', image: 'Images/cockatoo.png' },
+	{ name: 'toucan', image: 'Images/toucan.png' },
 ];
 
 //initial Time
-let seconds = 0 ;
+let seconds = 0;
 minutes = 0;
 
 // Initial moves and win
@@ -35,111 +35,104 @@ let movesCount = 0;
 winCount = 0;
 
 //For timer
-const timeGenerator = ()=>{
-  seconds += 1;
-  //minutes logic
+const timeGenerator = () => {
+	seconds += 1;
+	//minutes logic
 
-  if(seconds >=60){
-    minutes += 1;
-    seconds = 0;
-
-  }
-//format time before displaying
-let secondsValue = seconds < 10 ? `0${seconds}`:seconds;
-let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
-timevalue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
-}
+	if (seconds >= 60) {
+		minutes += 1;
+		seconds = 0;
+	}
+	//format time before displaying
+	let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+	let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+	timevalue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
 
 //For calculating moves
-const movesCounter = () =>{
-  movesCount += 1;
-  moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+const movesCounter = () => {
+	movesCount += 1;
+	moves.innerHTML = `<span>Moves:</span>${movesCount}`;
 };
 
 //Pick random objects from the ittems array
 
-const generateRandom = (size = 4) =>{
-  let tempArray = [...items];
-  let cardvalues = [];
-  size = (size*size)/2;
-  for(let i =0; i < size ; i++){
-    const randomIndex = Math.floor(Math.random()*tempArray.length);
-    cardvalues.push(tempArray[randomIndex]);
-    tempArray.splice(randomIndex,1);
-  }
-  return cardvalues;
-}
+const generateRandom = (size = 4) => {
+	let tempArray = [...items];
+	let cardvalues = [];
+	size = (size * size) / 2;
+	for (let i = 0; i < size; i++) {
+		const randomIndex = Math.floor(Math.random() * tempArray.length);
+		cardvalues.push(tempArray[randomIndex]);
+		tempArray.splice(randomIndex, 1);
+	}
+	return cardvalues;
+};
 
-const matrixGenerator = (cardValues,size = 4) => {
-  gameContainer.innerHTML = "";
-  cardValues = [...cardValues,...cardValues];
+const matrixGenerator = (cardValues, size = 4) => {
+	gameContainer.innerHTML = '';
+	cardValues = [...cardValues, ...cardValues];
 
-  cardValues.sort(() => Math.random() - 0.5);
-  for (let i = 0;i<size *size ; i++){
-  
-   gameContainer.innerHTML +=`
+	cardValues.sort(() => Math.random() - 0.5);
+	for (let i = 0; i < size * size; i++) {
+		gameContainer.innerHTML += `
    <div class="card-container" data-card-value ="${cardValues[i].name}">
       <div class ="card-before">?</div>
       <div class="card-after">
       <img src="${cardValues[i].image}" class="image"/></div>
    </div>
-   ` ;
-  }
-  //Grid
-gameContainer.style.gridTemplateColumns = `repeat(${size},auto`;
+   `;
+	}
+	//Grid
+	gameContainer.style.gridTemplateColumns = `repeat(${size},auto`;
 
-//cards
-cards = document.querySelectorAll(".card-container");
-cards.forEach((card) =>{
-  card.addEventListener("click",()=>{
-    if(!card.classList.contains("matched")){
-      card.classList.add("flipped");
-      if(!firstcard){
-        firstcard = card;
-        firstCardValue= card.getAttribute("data-card-value");
-        
-      }
-      else{
-        movesCounter();
-        secondCard = card;
-        let secondCardValue = card.getAttribute("data-card-value");
-        if(firstCardValue == secondCardValue){
-          firstcard.classList.add("matched");
-          secondCard.classList.add("matched");
-          firstcard = false;
-          winCount += 1;
-          if(winCount == Math.floor(cardValues.length/2)){
-            result.innerHTML = `<h2> You Won</h2><h4>Moves: ${movesCount}</h4`;
-            stopGame();
-          }
-        }else{
-         
-          let [tempFirst , tempSecond] = [firstcard,secondCard];
-          firstcard = false;
-          secondCard = false;
-          let delay = setTimeout(() => {
-            tempFirst.classList.remove("flipped");
-            tempSecond.classList.remove("flipped");
-          }, 900);
-        }
-      }
-    }
-   
-  });
-});
-
+	//cards
+	cards = document.querySelectorAll('.card-container');
+	cards.forEach((card) => {
+		card.addEventListener('click', () => {
+			if (!card.classList.contains('matched')) {
+				card.classList.add('flipped');
+				if (!firstcard) {
+					firstcard = card;
+					firstCardValue = card.getAttribute('data-card-value');
+				} else {
+					movesCounter();
+					secondCard = card;
+					let secondCardValue = card.getAttribute('data-card-value');
+					if (firstCardValue == secondCardValue) {
+						firstcard.classList.add('matched');
+						secondCard.classList.add('matched');
+						firstcard = false;
+						winCount += 1;
+						if (winCount == Math.floor(cardValues.length / 2)) {
+							result.innerHTML = `<h2> You Won</h2><h4>Moves: ${movesCount}</h4`;
+							stopGame();
+						}
+					} else {
+						let [tempFirst, tempSecond] = [firstcard, secondCard];
+						firstcard = false;
+						secondCard = false;
+						let delay = setTimeout(() => {
+							tempFirst.classList.remove('flipped');
+							tempSecond.classList.remove('flipped');
+						}, 900);
+					}
+				}
+			}
+		});
+	});
 };
 
 //start game
-startButton.addEventListener("click",() =>{
-  movesCount= 0;
-  time = 0;
-  controls.classList.add("hide");
-  stopButton.classList.remove("hide");
-  startButton.classList.add("hide");
-  interval = setInterval(timeGenerator,1000);
-  moves.innerHTML = `<span> Moves: </span> ${movesCount}`;
-  initializer(); 
+startButton.addEventListener('click', () => {
+	movesCount = 0;
+	time = 0;
+	controls.classList.add('hide');
+	stopButton.classList.remove('hide');
+	startButton.classList.add('hide');
+	interval = setInterval(timeGenerator, 1000);
+	moves.innerHTML = `<span> Moves: </span> ${movesCount}`;
+	initializer();
 });
 
 
